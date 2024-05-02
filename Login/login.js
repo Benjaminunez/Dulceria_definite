560207b5ac7e4df81539c5ce34c45d303ff5fc8d
Al3xm4b0y <142906642+Al3xm4b0y@users.noreply.github.com>
2024-05-02 16:30:30 -0400

    agregado avance login

---
+++ Login/login.js
@@ -0,0 +1,14 @@
+function validarUser(){
+    let input=document.querySelector("#User");
+    if(input.value.length >= 10){
+        input.classList.add("correct");
+        input.classList.remove("incorrect");
+        document.querySelector("#error-User").innerHTML = "&nbsp;";
+        
+    }else{
+        input.classList.add("incorrect");
+        input.classList.remove("correct");
+        document.querySelector("#error-user").innerHTML
+         = "Error, ingrese minimo 10 caracteres!.";
+    }
+}
