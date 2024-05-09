4371a6bb6b2b440a2833b0e152a58a5ae1f94228
Nayco <n.jerez@duocuc.cl>
2024-05-09 16:17:51 -0400

    validaciones_definitivas

--- Login/login.js
+++ Login/login.js
@@ -1,15 +1,27 @@
-function validarUser(){
-    let input=document.querySelector("#User");
-    if(input.value.length >= 10){
-        input.classList.add("correct");
-        input.classList.remove("incorrect");
-        document.querySelector("#error-User").innerHTML = "&nbsp;" ;
-        
-        
-    }else{
-        input.classList.add("incorrect");
-        input.classList.remove("correct");
-        document.querySelector("#error-user").innerHTML
-         = "Error, ingrese minimo 10 caracteres!.";
+function validarCampos() {
+  let campos = document.querySelectorAll(".form-control");
+  for (let i = 0; i < campos.length; i++) {
+    if (campos[i].value == "") {
+      document.querySelector("#error").innerHTML = "!Campos vacios¡";
+      return false;
     }
+
+  }
+  return true;
+}
+
+function validarUser() {
+  let input = document.querySelector("#User");
+  if (input.value.length >= 10) {
+    input.classList.add("correct");
+    input.classList.remove("incorrect");
+    document.querySelector("#error-user").innerHTML = "&nbsp;";
+
+
+  } else {
+    input.classList.add("incorrect");
+    input.classList.remove("correct");
+    document.querySelector("#error-user").innerHTML
+      = "Error, ingrese minimo 10 caracteres!.";
+  }
 }
