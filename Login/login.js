c800732133bbf7a4a26efab843dcaa64ebb3d0b3
Nayco <n.jerez@duocuc.cl>
2024-05-09 15:26:12 -0400

    validacion_campos_vacios_Nico

--- Login/login.js
+++ Login/login.js
@@ -1,44 +1,11 @@
-const form = document.getElementById("log_in");
-const errorMessage = document.getElementById("Password");
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const User = document.getElementById("User").value;
-  const password = document.getElementById("Password").value;
-
-  if (username === '' || password === '') {
-    errorMessage.innerText = '!!!!!Por favor, complete todos los campos¡¡¡¡¡¡';
-    return;
-  }
-
-  if (password.length < 4) {
-    errorMessage.innerText = '!!!!!!La contraseña debe tener al menos 4 caracteres¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡';
-    return;
-  }
-
-   if (!/^[a-zA-Z0-9]+$/.test(username)) {
-    errorMessage.innerText = '!!!!!!!!El usuario solo puede contener letras y números¡¡¡¡¡¡¡';
-    return;
-  }
-
-  const users = [
-    { username: 'admin', password: 'password123' },
-    { username: 'user', password: 'password456' }
-  ];
-
-  let isValid = false;
-  for (const user of users) {
-    if (user.username === username && user.password === password) {
-      isValid = true;
-      break;
+function validarCampos(){
+  let campos = document.querySelectorAll(".form-control");
+  for(let i=0; i<campos.length; i++){
+    if(campos[i].value == ""){
+      document.querySelector("#error").innerHTML = "!Campos vacios¡";
+      return false;
     }
-  }
 
-  if (!isValid) {
-    errorMessage.innerText = 'Usuario Incorrecto';
-    return;
   }
-
-  alert('Autenticación exitosa!');
-});
+  return true;
+}
